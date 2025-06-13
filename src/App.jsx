8fbc6ceb2fd9f5aef3c2a11f26955e0e2213fdaa
IzaakBrough <116444798+IzaakBrough@users.noreply.github.com>
2025-06-13 11:45:46 +0100

    setup dependencies + made text darker

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
   return (
     <Provider>
       <AppContextProvider>
-        <Box bg="gray.100">
+        <Box bg="gray.100" color="gray.800">
           <Router>
             <Routes>
               <Route
